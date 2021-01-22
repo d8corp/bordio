01146f489aa5516528b1f8b6750488a67cac268a
Mike Lysikov <d8corp@mail.ru>
2021-01-22 11:46:06 +0300

    moved registration fields to an addition file

--- src/forms/RegistrationForm/RegistrationForm.tsx
+++ src/forms/RegistrationForm/RegistrationForm.tsx
@@ -8,11 +8,11 @@ import registration from 'src/api/registration'
 // components
 import Field, {IOnFieldChange} from 'src/components/Field'
 import Button from 'src/components/Button'
-import Link from 'src/components/Link'
 
 // file imports
 import emailImage from './email.svg'
 import passwordImage from './password.svg'
+import registrationFormFields from './registrationFormFields'
 
 // style imports
 import './RegistrationForm.css'
@@ -36,65 +36,7 @@ const beforeIcons: Record<string, ReactNode> = {
  * */
 class RegistrationForm extends Component<{}, IRegistrationFormState> {
   state = {
-    fields: [
-      {
-        name: 'name',
-        type: 'text',
-        placeholder: 'Enter your name',
-        value: undefined,
-        error: '',
-        required: true,
-        pattern: '^[a-zA-Z]+$',
-      },
-      {
-        name: 'email',
-        type: 'email',
-        placeholder: 'Email',
-        value: undefined,
-        error: '',
-        required: true,
-        pattern: '^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
-        patternError: 'Please enter a valid email address',
-      },
-      {
-        name: 'password',
-        type: 'password',
-        placeholder: 'Password',
-        value: undefined,
-        error: '',
-        required: true,
-        pattern: '.{6,}',
-        patternError: 'Password must contain at least 6 symbols',
-      },
-      {
-        name: 'country',
-        type: 'select',
-        placeholder: 'Select country',
-        value: undefined,
-        values: ['Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya'],
-        error: '',
-        required: true,
-        requiredError: 'You must select your country',
-      },
-      {
-        name: 'gender',
-        type: 'radiobox',
-        value: undefined,
-        values: ['MALE', 'FEMALE'],
-        error: '',
-        required: true,
-        override: (val: string) => val === 'MALE' ? 'Male' : 'Female'
-      },
-      {
-        name: 'policies',
-        type: 'checkbox',
-        placeholder: <>Accept <Link href='#'>terms</Link> and <Link href='#'>conditions</Link></>,
-        value: false,
-        error: '',
-        required: true,
-        requiredError: 'You must accept the policies',
-      }
-    ],
+    fields: registrationFormFields,
     disabled: true,
     loading: false,
   }
---
+++ src/forms/RegistrationForm/registrationFormFields.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Link from 'src/components/Link'
+
+export default [
+  {
+    name: 'name',
+    type: 'text',
+    placeholder: 'Enter your name',
+    value: undefined,
+    error: '',
+    required: true,
+    pattern: '^[a-zA-Z]+$',
+  },
+  {
+    name: 'email',
+    type: 'email',
+    placeholder: 'Email',
+    value: undefined,
+    error: '',
+    required: true,
+    pattern: '^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
+    patternError: 'Please enter a valid email address',
+  },
+  {
+    name: 'password',
+    type: 'password',
+    placeholder: 'Password',
+    value: undefined,
+    error: '',
+    required: true,
+    pattern: '.{6,}',
+    patternError: 'Password must contain at least 6 symbols',
+  },
+  {
+    name: 'country',
+    type: 'select',
+    placeholder: 'Select country',
+    value: undefined,
+    values: ['Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya'],
+    error: '',
+    required: true,
+    requiredError: 'You must select your country',
+  },
+  {
+    name: 'gender',
+    type: 'radiobox',
+    value: undefined,
+    values: ['MALE', 'FEMALE'],
+    error: '',
+    required: true,
+    override: (val: string) => val === 'MALE' ? 'Male' : 'Female'
+  },
+  {
+    name: 'policies',
+    type: 'checkbox',
+    placeholder: <>Accept <Link href='#'>terms</Link> and <Link href='#'>conditions</Link></>,
+    value: false,
+    error: '',
+    required: true,
+    requiredError: 'You must accept the policies',
+  }
+]
